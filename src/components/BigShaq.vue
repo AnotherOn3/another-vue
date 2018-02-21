ce3b238830e977a6dac5cc05af080fc107ae1913
aleksandarmihaylov <aleksandaryanev95@gmail.com>
2018-02-21 14:13:37 +0100

    Big shaq is there

--- src/components/BigShaq.vue
+++ src/components/BigShaq.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Hello BigShaq</h1>
+  <h1>Hello {{ name }}</h1>
 </template>
+
+<script>
+export default {
+  // this is how you can use it as custom html tag, this will be the name of your component
+  name: 'BigShaq',
+  data() {
+    return {
+      name: 'BigShaq',
+    };
+  },
+};
+</script>
